7917dfa228c08834cca87b6206b20f2bbe94051b
Debby <34969423+Deb0006@users.noreply.github.com>
2023-12-04 22:01:47 -0500

    Update Layout for pages

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,16 @@
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import { Container} from '@mui/material';
+
 export default function Layout({ children }) {
+
     return (
       <div>
         <Navbar />
+        <Container sx={{paddingTop: 2,
+    paddingBottom: 3, maxWidth: 'lg'}}>
         {children}
+      </Container>
         <Footer />
       </div>
     );
