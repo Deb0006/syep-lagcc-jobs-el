0fa959ce2434e2e9d9f5da1bc655224af71fb2c1
Debby <34969423+Deb0006@users.noreply.github.com>
2023-12-04 22:01:24 -0500

    Adds JobCard component

---
+++ src/components/JobCard.js
@@ -0,0 +1,130 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Grid from "@mui/material/Grid";
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import HealthAndSafetyOutlinedIcon from '@mui/icons-material/HealthAndSafetyTwoTone';
+import CampaignTwoToneIcon from '@mui/icons-material/CampaignTwoTone';
+import StorefrontTwoToneIcon from '@mui/icons-material/StorefrontTwoTone';
+import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
+import CategoryOutlinedIcon from '@mui/icons-material/CategoryOutlined';
+import PersonSearchOutlinedIcon from '@mui/icons-material/PersonSearchOutlined';
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+const icons = {
+  HealthAndSafetyOutlinedIcon: HealthAndSafetyOutlinedIcon,
+  CampaignTwoToneIcon: CampaignTwoToneIcon,
+  StorefrontTwoToneIcon: StorefrontTwoToneIcon,
+  //...other icons  
+};
+export default function JobCard(props) {
+  const [expanded, setExpanded] = React.useState(false);
+ 
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  const Icon = icons[props.iconName] || null;
+
+
+  return ( 
+    <Card sx={{ maxWidth: 345, border: '1px solid lightgrey', boxShadow: 'none' }}>
+      <CardHeader
+        avatar={
+          // <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+          //   R
+          // </Avatar>
+          <Avatar sx={{ bgcolor: '#215d6e' }} aria-label="avatar">
+            {Icon && <Icon />}
+          </Avatar>
+
+        }
+        action={
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+        }
+        title={props.title}
+        subheader={props.company}
+      />
+      {/* <CardMedia
+        component="img"
+        height="194"
+        image={props.link}
+        alt="Paella dish"
+      /> */}
+      <CardActions disableSpacing>
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+        
+          <LocationOnOutlinedIcon color="primary"
+          sx={{
+            fontSize: 16, 
+            strokeWidth: 2, 
+            marginBottom: -0.3, 
+          }}/>{props.zipcode} 
+          <CategoryOutlinedIcon color="primary" 
+          sx={{
+            fontSize: 16, 
+            strokeWidth: 2, 
+            marginBottom: -0.3, 
+          }}/>{props.jobCategory}
+          {props.backgroundCheck === true && (
+          <>
+            <PersonSearchOutlinedIcon
+            color="primary"
+              sx={{
+                fontSize: 16,  // Adjust the size as needed
+                strokeWidth: 2,  // Adjust the thickness of the lines
+                marginBottom: -0.4,  // Add space between the job category and icon
+              }}
+            />
+            {'Background Check Required'}
+          </>
+        )}
+        
+      
+        </Typography>
+        
+      </CardContent>
+    
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Description:</Typography>
+          <Typography paragraph>
+            {props.description}
+          </Typography>
+          
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+}
