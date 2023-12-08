c64b2459ab975dddfba86549447ed52911814d70
Debby <34969423+Deb0006@users.noreply.github.com>
2023-12-08 04:26:15 -0500

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,10 +7,14 @@ export default function Layout({ children }) {
     return (
       <div>
         <Navbar />
-        <Container sx={{paddingTop: 2,
-    paddingBottom: 3, maxWidth: 'lg'}}>
-        {children}
-      </Container>
+        <Container 
+          sx={{paddingTop: 2,
+          paddingBottom: 3, 
+          maxWidth: 'lg',
+          margin: '0 auto', // Centers the container horizontally
+          }}>
+          {children}
+        </Container>
         <Footer />
       </div>
     );
