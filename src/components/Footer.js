172f98c83efc4a7336dcb917a3a65fb0b7969d3c
Debby <34969423+Deb0006@users.noreply.github.com>
2023-12-11 00:21:51 -0500

    Add info to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,24 @@
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+
 export default function Footer() {
-    return (
-        <div>footer</div>
-    )
-  }
+  return (
+    <Box bgcolor="#9AA2C9" height={100} sx={{padding:'5'}}>
+      <Typography
+        style={{
+          fontWeight: "bold",
+        }}
+      >
+        CONTACT US Phone: (718) 482-5340 (718) 609-2132
+      </Typography>
+      <Typography
+        style={{
+          fontWeight: "bold",
+        }}
+      >
+        Email: SYEP (All SYEP participant and work partner inquiries):
+        syep@lagcc.cuny.edu
+      </Typography>
+    </Box>
+  );
+}
