0e4e9c309c2dcbe7fb1ee3e178908e9fb87c0c9f
Debby <34969423+Deb0006@users.noreply.github.com>
2023-12-20 10:24:10 -0500

    made Slideshow Paper containers bigger

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -16,29 +16,29 @@ export default function Slideshow() {
       ];
 
     return (
-      <Box sx={{ width: "100%" }}>
+      <Box sx={{ width: "100%", border: '1px solid red' }}>
         <List component={Stack} direction="row">
           {images.map((image, index) => (
-            <ListItem key={index} sx={{ paddingLeft: "0" }}>
+            <ListItem key={index} sx={{ paddingLeft: "0", paddingRight: "30px" }}>
               <Paper
                 elevation={0}
                 style={{
-                  width: 160,
-                  height: 160,
+                  width: 180,
+                  height: 180,
                   padding: "25px",
                   display: "flex",
                   alignItems: "center",
                   justifyContent: "center",
                   background:
-                  "linear-gradient(135deg, rgba(244, 208, 255, 0.40) 0%, rgba(147, 184, 255, 0.32) 60.06%)",
-                  backdropFilter: 'blur(5px)',
+                    "linear-gradient(131deg, #75C4D9 0%, #EDB2D0 96.8%)",
+                  backdropFilter: "blur(5px)",
                 }}
               >
                 <Image
                   src={image}
                   alt={`Image ${index + 1}`}
-                  width={100}
-                  height={100}
+                  width={120}
+                  height={120}
                   style={{
                     width: "100%",
                     height: "100%",
