86536cca6124dc46cd45f34c5a571f60b5c5f001
Debby <34969423+Deb0006@users.noreply.github.com>
2024-02-13 20:49:47 -0500

    Move theme.js to styles

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,37 +1,37 @@
-import '../styles/globals.css'
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
+import "../styles/globals.css";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import Head from "next/head";
-import theme from './theme'
+import theme from "../styles/theme";
 import React, { useState, createContext } from "react";
 
 export const DialogContext = createContext();
 
-  function App({ Component, pageProps }) {
-    const [open, setOpen] = useState(false);
+function App({ Component, pageProps }) {
+  const [open, setOpen] = useState(false);
 
-    const handleOpen = () => {
-      setOpen(true);
-    };
+  const handleOpen = () => {
+    setOpen(true);
+  };
 
-    const handleClose = () => {
-      setOpen(false);
-    };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-    return (
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-          />
-        </Head>
-        <DialogContext.Provider value={{ open, handleOpen, handleClose }}>
-          <Component {...pageProps} />
-        </DialogContext.Provider>
-      </ThemeProvider>
-    );
-  }
-  
-  export default App;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+      </Head>
+      <DialogContext.Provider value={{ open, handleOpen, handleClose }}>
+        <Component {...pageProps} />
+      </DialogContext.Provider>
+    </ThemeProvider>
+  );
+}
+
+export default App;
