d10d07b6cc37cf7f317ca7ad7f012820e2402306
Debby <34969423+Deb0006@users.noreply.github.com>
2024-02-15 20:10:10 -0500

    Fix hydration error on Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,11 +15,9 @@ export default function Footer() {
       <Divider />
       <Container height={100} sx={{ padding: "20px" }}>
         <Grid container spacing={2}>
-          {" "}
           {/* Use Grid container */}
           {/* Left column: Information about SYEP */}
           <Grid item xs={12} sm={6}>
-            {" "}
             {/* Adjust column width for different screen sizes */}
             <Typography variant="subtitle1" style={{ fontWeight: "bold" }}>
               About SYEP in NYC
@@ -33,74 +31,78 @@ export default function Footer() {
           </Grid>
           {/* Right column: Links */}
           <Grid item xs={12} sm={6}>
-            {" "}
             {/* Adjust column width for different screen sizes */}
             <Typography variant="subtitle1" style={{ fontWeight: "bold" }}>
               Contact Information
             </Typography>
-            <Typography variant="body1">
-              <ul >
-                <li>
-                  <PhoneIcon
-                    fontSize="small"
-                    style={{ verticalAlign: "middle", marginRight: "4px" }}
-                  />
+            <ul style={{ listStyle: "none" }}>
+              <li>
+                <Typography sx={{ color: "#38556A" }}>
+                <PhoneIcon
+                  fontSize="medium"
+                  style={{ verticalAlign: "middle", marginRight: "4px" }}
+                />
                   (718) 482-5340 | (718) 609-2132
-                </li>
-                <li>
+                </Typography>
+              </li>
+              <li>
+                <Link href="mailto:syep@lagcc.cuny.edu">
                   <EmailIcon
-                    fontSize="small"
+                    fontSize="medium"
                     style={{ verticalAlign: "middle", marginRight: "4px" }}
                   />
-                  <a href="mailto:syep@lagcc.cuny.edu">syep@lagcc.cuny.edu</a>
-                </li>
-                <li>
-                  <Link
-                    href="https://www.instagram.com/syep_laguardia/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <InstagramIcon
-                      fontSize="small"
-                      style={{ verticalAlign: "middle", marginRight: "4px" }}
-                    />
-                    SYEP NYC on Instagram
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="https://www.linkedin.com/company/laguardia-community-college-workforce-educationcenter/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <LinkedInIcon
-                      fontSize="small"
-                      style={{ verticalAlign: "middle", marginRight: "4px" }}
-                    />
-                    SYEP NYC on LinkedIn
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="https://www.facebook.com/syeplaguardia/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <FacebookIcon
-                      fontSize="small"
-                      style={{ verticalAlign: "middle", marginRight: "4px" }}
-                    />
-                    SYEP NYC on Facebook
-                  </Link>
-                </li>
-                <li>
-                  Government Website:{" "}
-                  <a href="https://www.nyc.gov/site/dycd/services/jobs-internships/summer-youth-employment-program-syep.page">
-                    SYEP - NYC.gov
-                  </a>
-                </li>
-              </ul>
-            </Typography>
+                  syep@lagcc.cuny.edu
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="https://www.instagram.com/syep_laguardia/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <InstagramIcon
+                    fontSize="medium"
+                    style={{ verticalAlign: "middle", marginRight: "4px" }}
+                  />
+                  Follow us on Instagram
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="https://www.linkedin.com/company/laguardia-community-college-workforce-educationcenter/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <LinkedInIcon
+                    fontSize="medium"
+                    style={{ verticalAlign: "middle", marginRight: "4px" }}
+                  />
+                  Follow us on LinkedIn
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="https://www.facebook.com/syeplaguardia/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FacebookIcon
+                    fontSize="medium"
+                    style={{ verticalAlign: "middle", marginRight: "4px" }}
+                  />
+                  Follow us on Facebook
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="https://www.nyc.gov/site/dycd/services/jobs-internships/summer-youth-employment-program-syep.page"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  SYEP - NYC.gov
+                </Link>
+              </li>
+            </ul>
           </Grid>
         </Grid>
       </Container>
